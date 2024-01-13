e7d5eb4225cab66798a03b7632b1fa4440823717
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2024-01-13 15:25:49 +0530

    get product by name

--- Express_basics/express_basic.js
+++ Express_basics/express_basic.js
@@ -18,6 +18,18 @@ app.get('/getAllProducts', function (req, res) {
   res.send(products)
 })
 
+// API endpoint to get details of a specific product
+app.get('/product/:name', (req, res) => {
+  const productName = req.params.name;
+  const product = products.find(p => p.name.toLowerCase() === productName.toLowerCase());
+
+  if (product) {
+      res.json(product);
+  } else {
+      res.status(404).send('Product not found!');
+  }
+});
+
 
 // The Port is 3000 where we are listening requests
 app.listen(3000,() => {
