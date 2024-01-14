245809d3a308c8de061c0ad56825b85008744ddb
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2024-01-14 16:13:07 +0530

    added PUT method

--- GroceryAPIs/server.js
+++ GroceryAPIs/server.js
@@ -1,24 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
 app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Middleware to parse JSON bodies
 
 // Assuming you have exported the data array from groceryData.js like this:
 // module.exports = groceryStoreData;
-const groceryStoreData = require('./groceryData.js');
+const groceryStoreData = require("./groceryData.js");
 
 // API to get all products
-app.get('/getAllProducts', (req, res) => {
+app.get("/getAllProducts", (req, res) => {
   res.status(200).json(groceryStoreData);
 });
 
 // POST API to add a new product
-app.post('/addProduct', (req, res) => {
-    const newProduct = req.body;
-    console.log(newProduct)
-    groceryStoreData.push(newProduct);
-    res.status(201).send('Product added successfully');
+app.post("/addProduct", (req, res) => {
+  const newProduct = req.body;
+  console.log(newProduct);
+  groceryStoreData.push(newProduct);
+  res.status(201).send("Product added successfully");
+});
+
+//PUT API to update a product based on ID
+app.put("/updateProduct/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedProduct = req.body;
+  const productIndex = groceryStoreData.findIndex((i) => i.id === parseInt(id));
+
+  if (productIndex != -1) {
+    groceryStoreData[productIndex] = {
+      ...groceryStoreData[productIndex],
+      ...updatedProduct,
+    };
+    res.send("Product updated successfully");
+  } else {
+    res.status(404).send("Product not found");
+  }
 });
 
 app.listen(port, () => {
